d13f7e628381004502dd815747f072aa2dc1ed32
Dyfeomorfizm <ximixd@gmail.com>
2023-09-08 21:49:22 +0200

    new model class

--- MDP model.py
+++ MDP model.py
@@ -1,71 +1,59 @@
-from random import randint,seed
+import random
 
 import pandas as pd
 
-
-
 excel = pd.ExcelFile("C:\\Users\\Ludwiczek Kroliczek\\PycharmProjects\\Diety\\diety\\diety\\Data\\Copy_of_MyFoodData.xlsx")
 dane = pd.read_excel(excel, "SR Legacy and FNDDS")
-list_of_columns = [dane["Name"], dane["Calories"]]
-dane_testowe = dane[["Name", "Calories"]]
-seed(12)
-random_list = []
-for i in range(0, 30):
-    random_list.append(randint(0, 14165))
-food_df = dane_testowe.take(random_list)
-food_df = food_df.reset_index(drop=True)
-print(food_df)
-seed()
+dane_testowe = dane.sample(n=30, random_state=12).reset_index(drop=True)
 
 
 class Model:
-    def __init__(self, dane, target, learning_rate, epsilon):
-        self.dane = self.add_qualities(dane)
+    def __init__(self, data, target, learning_rate, epsilon, episodes, num_ingredients, starting_qualities=0):
+        self.data = data
         self.target = target
         self.learning_rate = learning_rate
         self.epsilon = epsilon
+        self.episodes = episodes
+        self.num_ingredients = num_ingredients
+        self.starting_qualities = starting_qualities
 
-    def add_qualities(self, dane):
-        dane["Qualites"] = 0.5
-        return dane
+    def get_available_data(self, data, picks):
+        return data[~data.isin(picks)].dropna()
 
-    def modelv1(self, dane, target, epsilon):
-        list_of_products = []
-        e = randint(1, 101)
-        while len(list_of_products) < 4:
-            list_of_random = []
-            for i in range(0, 3):
-                list_of_random.append(randint(0, 29))
-            list_of_qualites = [dane["Qualites"][i] for i in list_of_random]
-            if list_of_qualites[0] == list_of_qualites[1] == list_of_qualites[2] == list_of_qualites[3]:
-                index = randint(0, 3)
-                list_of_products.append(list_of_random[index])
-            elif epsilon > e:
-                index = randint(0,3)
-                list_of_products.append(list_of_random[index])
-            else:
-                index = list_of_qualites.index(max(list_of_qualites))
-                list_of_products.append(list_of_random[index])
-        sum = 0
-        for i in list_of_products:
-            sum += dane["Calories"][i]
-        diff = target - sum
-        return diff, list_of_products
+    def get_highest_value(self, df, column_name):
+        return df.filter([df[column_name].idxmax()], axis=0)
 
-    def reward(self, diff):
-        if diff <= 0:
+    def get_reward(self, calories):
+        if self.target - calories <= 0:
             return -1
         else:
             return 1
-    def update(self, dane,reward,learning_rate, list_of_products):
-        for i in list_of_products:
-            dane["Qualites"][i] = dane["Qualites"][i] + learning_rate*(reward - dane["Qualites"][i])
 
+    def get_update(self, data, prev_episode, reward):
+        return data[f"V_{prev_episode}"] + self.learning_rate * (reward - data[f"V_{prev_episode}"])
 
+    def train(self):
+        data = self.data.copy()
+        data["V_0"] = self.starting_qualities
+        for episode in range(1, self.episodes + 1):
+            picks = pd.DataFrame()
+            if episode > 1:
+                for ingredient in range(self.num_ingredients):
+                    avails = self.get_available_data(data, picks)
+                    if random.random() < self.epsilon:
+                        picks = picks.append(avails.sample(1))
+                    else:
+                        picks = picks.append(self.get_highest_value(avails, f"V_{episode - 1}"))
 
+            else:
+                picks = picks.append(data.sample(self.num_ingredients))
 
-
-
-
-
+            calories = picks.Calories.sum()
+            reward = self.get_reward(calories)
+            data[f"V_{episode}"] = data[f"V_{episode - 1}"].where(
+                cond=~data.index.isin(picks.index),
+                other=self.get_update(data, episode - 1, reward)
+            )
+            data = data.copy()
+        return data
 
