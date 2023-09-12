144621d753696a21776fa72b3cd52bb0bbd6c41a
unknown <R0L0G>
2023-09-12 11:39:17 +0200

    First iteration of the model proceed to build better one

--- Model_V1.py
+++ Model_V1.py
@@ -1,7 +1,7 @@
+import random
 from random import randint,seed
 
 import pandas as pd
-
 
 
 excel = pd.ExcelFile("C:\\Users\\Ludwiczek Kroliczek\\PycharmProjects\\Diety\\diety\\diety\\Data\\Copy_of_MyFoodData.xlsx")
@@ -30,48 +30,49 @@ class Model:
 
     def modelv1(self, dane, target, epsilon):
         list_of_products = []
-        e = randint(1, 101)
+        e = random.random()
         while len(list_of_products) < 4:
             list_of_random = []
-            for i in range(0, 4):
+            for i in range(0, 6):
                 list_of_random.append(randint(0, 29))
             list_of_qualites = [dane["Qualites"][i] for i in list_of_random]
-            if list_of_qualites[0] == list_of_qualites[1] == list_of_qualites[2] == list_of_qualites[3]:
-                index = randint(0, 3)
+            if list_of_qualites[0] == list_of_qualites[1] == list_of_qualites[2] == list_of_qualites[3] == list_of_qualites[4] == list_of_qualites[5]:
+                index = randint(0, 4)
                 list_of_products.append(list_of_random[index])
             elif epsilon > e:
-                index = randint(0,3)
+                index = randint(0,4)
                 list_of_products.append(list_of_random[index])
             else:
                 index = list_of_qualites.index(max(list_of_qualites))
                 list_of_products.append(list_of_random[index])
         sum = 0
         for i in list_of_products:
-            sum += dane["Calories"][i]
+            sum += dane.loc[i, "Calories"]
         diff = target - sum
         return diff, list_of_products
 
     def reward(self, diff):
         if diff <= 0:
-            return -1
-        else:
             return 1
+        else:
+            return -1
     def update(self, dane, reward,learning_rate, list_of_products):
         for i in list_of_products:
-            dane["Qualites"][i] = dane["Qualites"][i] + learning_rate*(reward - dane["Qualites"][i])
+            dane.loc[i, "Qualites"] = dane.loc[i, "Qualites"] + learning_rate*(reward - dane.loc[i, "Qualites"])
 
     def evaluation(self, nb_episodes):
-        for i in range(0, nb_episodes-1):
+        nb_episodes_dic = {}
+        for i in range(0, nb_episodes):
             Model.modelv1(self, self.dane, 560, 0.5)
-            nb_episodes_dic = {f"Episode{i}": Model.modelv1(self, self.dane, 560, 0.5)[0]}
+            nb_episodes_dic[f"Episode {i}"] = Model.modelv1(self, self.dane, 560, 0.5)[0]
             Model.reward(self, Model.modelv1(self, self.dane, 560, 0.5)[0])
             Model.update(self, self.dane, Model.reward(self, Model.modelv1(self, self.dane, 560, 0.5)[0]) ,0.05, Model.modelv1(self, self.dane, 560, 0.5)[1])
         return nb_episodes_dic
 
 
 if __name__ == "__main__":
-    modelV1 = Model(food_df, 560, 0.05, 0.5)
-    Model.evaluation(modelV1, 500)
+    modelV1 = Model(food_df, 560, 0.05, 0.05)
+    print(Model.evaluation(modelV1, 500))
 
 
 
